b269b041a3ffd709bbe8a5676edfa377db68bb32
Sean Ippolito <ippolitosean@gmail.com>
2017-10-09 13:07:23 -0700

    Roll-A-Ball completed with .gitignore

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+	private int count;
+	public Text countText;
+	public Text winText;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal;
+		float moveVertical;
+
+		if (SystemInfo.deviceType == DeviceType.Desktop) {
+			moveHorizontal = Input.GetAxis ("Horizontal");
+			moveVertical = Input.GetAxis ("Vertical");
+		} else {
+			moveHorizontal = Input.acceleration.x;
+			moveVertical = Input.acceleration.y;
+		}
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Pickup")) 
+		{
+			other.gameObject.SetActive (false);
+			count++;
+
+			SetCountText ();
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) 
+		{
+			winText.text = "You Win! Biatch";
+		}
+	}
+}
